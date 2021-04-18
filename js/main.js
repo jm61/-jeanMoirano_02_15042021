df99a7de58753a9b01b7f9559ef1cadf7ba7d0c6
jm61 <jean.moirano@gmail.com>
2021-04-18 14:51:00 +0200

    final desktop

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn");
+      const btn2 = document.querySelector(".btn2");
+      const eco = document.querySelector("#eco");
+      btn.addEventListener("click", (e) => {
+        console.log("btn clicked");
+      });
+      btn2.addEventListener("click", (e) => {
+        console.log("btn2 clicked");
+      });
+      eco.addEventListener("click", (e) => {
+        console.log("Eco clicked");
+      });
